c609f6448109e73948a9308592cb3e144ab89b2f
Proterius <mlynarczykacper@gmail.com>
2022-03-25 21:00:11 +0100

    no3
    make elete from cart work!!!

---
+++ asd
@@ -0,0 +1,66 @@
+<!-- item no2 -->
+        <div class="item">
+          <img src="item.jpg" alt="" class="item-photo" />
+          <div class="description">
+            <div>
+              <p class="product-name">Jeans</p>
+              <span class="price">39.9$</span>
+            </div>
+            <div>
+              <button class="add-to-cart">🛒</button>
+            </div>
+          </div>
+        </div>
+        <!-- item no3 -->
+        <div class="item">
+          <img src="item.jpg" alt="" class="item-photo" />
+          <div class="description">
+            <div>
+              <p class="product-name">Jeans</p>
+              <span class="price">39.9$</span>
+            </div>
+            <div>
+              <button class="add-to-cart">🛒</button>
+            </div>
+          </div>
+        </div>
+        <!-- item no4 -->
+        <div class="item">
+          <img src="item.jpg" alt="" class="item-photo" />
+          <div class="description">
+            <div>
+              <p class="product-name">Jeans</p>
+              <span class="price">39.9$</span>
+            </div>
+            <div>
+              <button class="add-to-cart">🛒</button>
+            </div>
+          </div>
+        </div>
+        <!-- item no5 -->
+        <div class="item">
+          <img src="item.jpg" alt="" class="item-photo" />
+          <div class="description">
+            <div>
+              <p class="product-name">Jeans</p>
+              <span class="price">39.9$</span>
+            </div>
+            <div>
+              <button class="add-to-cart">🛒</button>
+            </div>
+          </div>
+        </div>
+        <!-- item no5 -->
+        <div class="item">
+          <img src="item.jpg" alt="" class="item-photo" />
+          <div class="description">
+            <div>
+              <p class="product-name">Jeans</p>
+              <span class="price">39.9$</span>
+            </div>
+            <div>
+              <button class="add-to-cart">🛒</button>
+            </div>
+          </div>
+        </div>
+        */
--- main.js
+++ main.js
@@ -9,6 +9,7 @@ let price;
 let opis;
 let img;
 let del;
+let suma = 0;
 let cartItemImg = document.querySelector(".cart-item-img");
 
 const createItemInCart = function () {
@@ -30,6 +31,7 @@ const createItemInCart = function () {
   del.classList.add("cart-item-del");
   element.appendChild(del);
 };
+
 // const addToCart = (e) =>{
 //     createItemInCart()
 //        if(e.target==input[0])console.log('casdasd');
@@ -38,24 +40,87 @@ const createItemInCart = function () {
 for (let i = 0; i < input.length; i++) {
   input[i].addEventListener("click", function (e) {
     createItemInCart();
+
     if (e.target == input[0]) {
+      img.innerHTML = productId1.image;
+      price.innerHTML += `Cena: ` + productId1.price + ` zł`;
+      opis.innerHTML = productId1.description;
+      del.innerHTML = `<button class="usun">❌</button>`;
+      console.log("1");
+      suma += productId1.price;
+      console.log(suma);
+      document.getElementById("suma").innerHTML = suma;
+    }
+    if (e.target == input[1]) {
+      img.innerHTML = productId2.image;
+      price.innerHTML += `Cena: ` + productId2.price + " zł";
+      opis.innerHTML = productId2.description;
+      del.innerHTML = `<button class="usun">❌</button>`;
+      console.log("1");
+      suma += productId2.price;
+      console.log(suma);
+      document.getElementById("suma").innerHTML = suma;
+    }
+    if (e.target == input[2]) {
+      img.innerHTML = productId3.image;
+      price.innerHTML += `Cena: ` + productId3.price + ` zł`;
+      opis.innerHTML = productId3.description;
+      del.innerHTML = `<button class="usun">❌</button>`;
+      console.log("1");
+      suma += productId3.price;
+      console.log(suma);
+      document.getElementById("suma").innerHTML = suma;
+    }
+    if (e.target == input[3]) {
       img.innerHTML = productId1.image;
       price.innerHTML = productId1.price;
       opis.innerHTML = productId1.description;
-      del.innerHTML = `<button id="usun">❌</button>`;
+      del.innerHTML = `<button class="usun">❌</button>`;
       console.log("1");
+      suma += productId1.price;
+      console.log(suma);
+      document.getElementById("suma").innerHTML = suma;
     }
-    if (e.target == input[1]) console.log("2");
-    if (e.target == input[2]) console.log("3");
-    if (e.target == input[3]) console.log("4");
-    if (e.target == input[4]) console.log("5");
-    if (e.target == input[5]) console.log("6");
+    if (e.target == input[4]) {
+      img.innerHTML = productId1.image;
+      price.innerHTML = productId1.price;
+      opis.innerHTML = productId1.description;
+      del.innerHTML = `<button class="usun">❌</button>`;
+      console.log("1");
+      suma += productId1.price;
+      console.log(suma);
+      document.getElementById("suma").innerHTML = suma;
+    }
+    if (e.target == input[5]) {
+      img.innerHTML = productId1.image;
+      price.innerHTML = productId1.price;
+      opis.innerHTML = productId1.description;
+      del.innerHTML = `<button class="usun">❌</button>`;
+      console.log("1");
+      suma += productId1.price;
+      console.log(suma);
+      document.getElementById("suma").innerHTML = suma;
+    }
   });
+
   console.log(input[i]);
 }
 const productId1 = {
-  description: "jeans",
-  price: Number(39.9),
-  image: `<img src="item.jpg" alt="" class="item-photo" />`,
+  description: "Blue Jeans Heart Pattern",
+  price: Number(199),
+  image: `<img src="jeans2.jpg" alt="" class="item-photo" />`,
 };
-console.log(productId1);
+const productId2 = {
+  description: "Carrot Jeans",
+  price: Number(99),
+  image: `<img src="jeans1.jpg" alt="" class="item-photo" />`,
+};
+const productId3 = {
+  description: "Blue Baggy Jeans",
+  price: Number(129),
+  image: `<img src="jeans3.jpg" alt="" class="item-photo" />`,
+};
+// console.log(productId1);
+document.getElementById("submit").addEventListener("click", function () {
+  alert("Payment acceptation: " + suma + "zł");
+});
